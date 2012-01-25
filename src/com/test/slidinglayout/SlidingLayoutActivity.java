52136a30e3820117330191a2fba063bf69ce83cd
Travis Churchill <travis@crowdcompass.com>
2012-01-25 12:07:58 -0800

    wip - trying to configure layout options

--- src/com/test/slidinglayout/SlidingLayoutActivity.java
+++ src/com/test/slidinglayout/SlidingLayoutActivity.java
@@ -1,5 +1,7 @@
 package com.test.slidinglayout;
 
+import com.spidermuffin.view.SlidingLayout;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -29,14 +31,13 @@ public class SlidingLayoutActivity extends Activity {
 		final DisplayMetrics dm = new DisplayMetrics();
 		m.getMetrics(dm);
     	int width = m.getWidth();
-//    	int height = m.getHeight();
-    	LinearLayout leftLayout = (LinearLayout) findViewById(R.id.left_layout);
-    	int leftWidth = (int) Math.floor(width * LEFT_LAYOUT_PERCENT_OF_SCREEN);
-//    	leftLayout.setLayoutParams(new RelativeLayout.LayoutParams(leftWidth , LayoutParams.MATCH_PARENT));
-    	LinearLayout rightLayout = (LinearLayout) findViewById(R.id.right_layout);
-//    	rightLayout.setLayoutParams(new RelativeLayout.LayoutParams(width, LayoutParams.MATCH_PARENT));
+//    	LinearLayout leftLayout = (LinearLayout) findViewById(R.id.left_layout);
+//    	int leftWidth = (int) Math.floor(width * LEFT_LAYOUT_PERCENT_OF_SCREEN);
+//    	leftLayout.setLayoutParams(new SlidingLayout.LayoutParams(width , LayoutParams.MATCH_PARENT));
+//    	LinearLayout rightLayout = (LinearLayout) findViewById(R.id.right_layout);
+//    	rightLayout.setLayoutParams(new SlidingLayout.LayoutParams(width, LayoutParams.MATCH_PARENT));
     	
-//    	RelativeLayout parent = (RelativeLayout) findViewById(R.id.test_linear_layout_view);
+//    	SlidingLayout parent = (SlidingLayout) findViewById(R.id.test_linear_layout_view);
 //    	parent.requestLayout();
 //    	parent.setMinimumWidth(leftWidth + width);
 //    	parent.setLayoutParams(new LinearLayout.LayoutParams(leftWidth + width, LinearLayout.LayoutParams.MATCH_PARENT));
